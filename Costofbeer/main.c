79dc2b6b7232d73de02bfe0660087c91e0d9db99
jhonkeithman123 <keithvirgenes17@gmail.com>
2025-12-04 20:40:05 +0800

    Fixed the rebase

---
+++ Costofbeer/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * 12 - Ounce of beer
+ * 
+ * 365 - 1 year
+ * 
+ * 15 pounds of calorie gain in one year
+ * from drinking 12 ounce of beer everyday.
+ * 
+ * beer-to-weight-gain = constant
+ * 
+ * Ask the user how many beers they expect to consume each day and also
+ * the amount of money theyy spend on it.
+ * 
+ * Goal: Write a program to determine:
+ * 1. How many beers will the user consume over the course of the year?
+ * 2. Assuming a beer is 150 calories,
+ *    how many calories will they take in from beer alone
+ *    over the course of the year?
+ * 3. How much weight can the user expect to gain in one year based
+ *    on the number of 12-ounce beers they consume every day?
+ * 4. How much the user will spend on beer this year?
+ * 
+ * All outputs should be 2 digits precision after the decimal point!
+ * Assume a year is 365 days (ignore leap years).
+ * 
+ * Input specification
+ * 1. The 12-ounces beers consumed each day will be a 
+ *    non-negative number.
+ * 2. The user can specify the brand of the beer and its price
+ *    because its not all the same for all people.
+ */
+
+int main() {
+
+    
+    
+    return 0;
+}
