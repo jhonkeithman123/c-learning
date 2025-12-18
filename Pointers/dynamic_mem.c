ce128d091112a2092ffce3c7cf66c39fcacc78f3
jhonkeithman123 <keithvirgenes17@gmail.com>
2025-12-18 17:41:18 +0800

    Improve the code in here

--- Pointers/dynamic_mem.c
+++ Pointers/dynamic_mem.c
@@ -2,12 +2,23 @@
 #include <stdlib.h>
 
 /**
- ** Make an array at runtime and I must use malloc() and free() to be able to complete it. 
+ ** Make an array at runtime and I must use malloc() and free() to be able to complete it.
  */
-int main() {
-    int n = 0;
-    int val = 0;
-    int inc = 0;
+
+/**
+ * Upgrades:
+ * Use calloc() instead of malloc() and observe how it initializes memory
+ * Expand the array with realloc() to double the its size and continue progression.
+ * Write a helper function fillArray(int *arr, int n, int val, int inc) to practice passing pointers
+ */
+
+void fillArray(int *arr, int n, int val, int inc);
+
+int main()
+{
+    int n = 0;   // Number of elements in an array
+    int val = 0; // Initial value of the array
+    int inc = 0; // Incremental multiplier of the
 
     printf("Enter the number of elements: ");
     scanf("%d", &n);
@@ -18,25 +29,30 @@ int main() {
     printf("Enter the increment size of the array: ");
     scanf("%d", &inc);
 
-    int *arr = (int *)malloc(n * sizeof(int));
-    if (arr == NULL) {
-        printf("Arr is null, returning... This will cause a segmentation fault.");
-        return 1;
+    int *arr = (int *)calloc(n, sizeof(int));
+    if (arr == NULL)
+    {
+        printf("Memory allocation for the arr variable failed, exiting...");
+        exit(0);
     }
 
-    for (int i = 0; i < n; i++) {
-        if (arr[0] == 0) {
-            arr[i] = val;
-        } else {
-            arr[i] = val * inc;
-        }
-    }
-    
-    for (int i = 0; i < n; i++) {
-        printf("arr[%d] = %d\n", i, arr[i]);
+    fillArray(arr, n, val, inc);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("*arr[%d] = %d\n", i, *(arr + i));
     }
 
     free(arr);
-    
+
     return 0;
+}
+
+void fillArray(int *arr, int n, int val, int inc)
+{
+    arr[0] = val;
+    for (int i = 1; i < n; i++)
+    {
+        arr[i] = arr[i - 1] * inc;
+    }
 }
